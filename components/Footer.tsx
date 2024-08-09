f9d70d871c6c78bdc5558500e42d0b822ea79f2c
dusousas <dusousadeveloper@gmail.com>
2024-08-09 13:13:28 -0300

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,13 @@
 const Footer = () => {
     return (
         <>
-            <footer id="contact" className="hrela py-4 bg-BrownP">
+            <footer id="contact" className="hrela py-4 bg-black">
                 <div className="maxWidth">
-                    <p className="text-center font uppercase font-semibold">Café Vó Leonilda - &copy; Todos os direitos reservados.</p>
-                    <p className="text-center">CNPJ - XX. XXX. XXX/0001-XX</p>
-
+                    <p className="text-center font uppercase text-white">Café Vó Leonilda - &copy; Todos os direitos reservados.</p>
+                    <p className="text-center text-sm text-white">CNPJ - XX. XXX. XXX/0001-XX</p>
+                    <div className="text-center">
+                        <span className="text-xs text-[#202020]">Desenvolvido por </span><a target="_blank" href="https://www.agenciayouon.com" className="text-xs underline text-[#202020]">You on</a>
+                    </div>
                 </div>
             </footer>
         </>
